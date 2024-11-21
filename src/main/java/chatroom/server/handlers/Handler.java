0022d4d8e7a696c2de9efe267f4d354d8f033ca5
Lehkdup <s.lehkdup@gmail.com>
2024-11-21 08:03:47 +0100

    Initial commit: Stand 0

---
+++ src/main/java/chatroom/server/handlers/Handler.java
@@ -0,0 +1,112 @@
+package chatroom.server.handlers;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * This class provides the top-level functionality for answering requests.
+ * It is extended by classes serving specific paths.
+ *
+ * Note: The two segments of code dealing with CORS are only necessary, if
+ * we are dealing with web clients. Browsers will block cross-origin requests,
+ * unless the server explicitly says that these are allowed.
+ */
+public abstract class Handler implements HttpHandler  {
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        try (// Get the input and output streams
+             BufferedReader in = new BufferedReader(new InputStreamReader(httpExchange.getRequestBody()));
+             OutputStreamWriter out = new OutputStreamWriter(httpExchange.getResponseBody())
+        ) {
+            // Web clients are sending cross-origin, because the client is not running on this server.
+            // In that case, the browser sends a pre-flight request, to ensure that a cross-origin
+            // request will be accepted. This is an OPTIONS command, and must be answered with headers
+            // that show what cross-origin commands are acceptable.
+            if (httpExchange.getRequestMethod().equalsIgnoreCase("OPTIONS")) {
+                httpExchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+                httpExchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+                httpExchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
+                httpExchange.sendResponseHeaders(204, -1); // No content for OPTIONS requests
+            } else { // For all other requests, our usual processing
+                // Empty response with an optimistic status-code
+                HandlerResponse response = new HandlerResponse();
+
+                String requestMethod = httpExchange.getRequestMethod();
+                if (requestMethod.equals("GET")) {
+                    handleGet(httpExchange, response);
+                } else if (requestMethod.equals("POST")) {
+                    JSONObject JSONin = readJSON(in);
+                    handlePost(httpExchange, JSONin, response);
+                } else { // Unsupported request type
+                    response.statusCode = 418;
+                    response.jsonOut.put("Error", "Invalid HTTP request method");
+                }
+
+                // We include the CORS headers for all normal requests as well,
+                // to ensure that web clients are happy.
+                httpExchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+                httpExchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+                httpExchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
+
+                // Send the response
+                String textOut = response.jsonOut.toString();
+                httpExchange.sendResponseHeaders(response.statusCode, textOut.length());
+                out.write(textOut);
+            }
+        }
+    }
+
+    /**
+     * The handler should override this method, if it supports GET-requests
+     */
+    protected void handleGet(HttpExchange httpExchange, HandlerResponse response) {
+        response.statusCode = 418;
+        response.jsonOut.put("Error", "Invalid HTTP request method");
+    }
+
+    /**
+     * The handler should override this method, if it supports POST-requests
+     */
+    protected void handlePost(HttpExchange httpExchange, JSONObject JSONin, HandlerResponse response) {
+        response.statusCode = 418;
+        response.jsonOut.put("Error", "Invalid HTTP request method");
+    }
+
+    /**
+     * Read the JSON from the input, and place into a JSONObject
+     */
+    protected JSONObject readJSON(BufferedReader in) {
+        JSONObject jsonIn = null;
+        try {
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = in.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            String jsonText = stringBuilder.toString();
+            jsonIn = new JSONObject(jsonText);
+        } catch (Exception e) {
+            // If anything goes wrong, return null
+        }
+        return jsonIn;
+    }
+
+    /**
+     * Helper method to read a JSON string. Getting exceptions is a nuisance - if a string does
+     * not exist, just return null.
+     */
+    protected String readString(JSONObject obj, String key) {
+        try {
+            return obj.getString(key);
+        } catch (JSONException e) {
+            return null;
+        }
+    }
+}
